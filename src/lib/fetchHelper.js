178739b9335b8ea1546f24dec3cb21c84f693a8f
rafiulaanam <rafiulaanam@gmail.com>
2023-08-29 02:31:01 +0600

    layout provider2

--- src/lib/fetchHelper.js
+++ src/lib/fetchHelper.js
@@ -1,5 +1,5 @@
 // Define the base URL for API requests
-const BASE_URL = "https://nextjs13-4-mongo-db-starter-4uldpgwch-rafiulaanam.vercel.app/";
+const BASE_URL = "https://nextjs13-4-mongo-db-starter.vercel.app/";
 
 // Function to fetch a list of users
 export const getData = async (route) => {
